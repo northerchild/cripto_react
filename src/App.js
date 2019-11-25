3970b58b8d5697fc2e0d94ef29d1795c4337c6ca
northerchild <osarus-13@hotmail.com>
2019-11-25 18:19:54 -0500

    Se agregan los archivos base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import imagen from './cryptomonedas.png'
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <img src={imagen} alt="imagen criptomoneda" className="logotipo"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
