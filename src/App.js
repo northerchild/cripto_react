e0ce6050777cb5058281c581b9ca41d12099fad3
northerchild <osarus-13@hotmail.com>
2019-11-28 14:35:16 -0500

    Se finaliza el proyecto

--- src/App.js
+++ src/App.js
@@ -1,44 +1,63 @@
-import React, {useState, useEffect}  from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import imagen from './cryptomonedas.png';
-import Formulario from '../src/components/Formulario'
-import Spinner from '../src/components/Spinner'
-import axios from 'axios'
+
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
+import Spinner from './components/Spinner';
+
 function App() {
-  const [moneda, guardarMoneda] = useState('');
-  const [criptomoneda, guardarCriptomoneda] = useState('');
-  const [cargando, guardarCargardo] = useState(false);
 
-  useEffect(()=>{
-    const cotizarCriptomoneda = async()=>{
-      //Si no hay moneda, no ejecutar
-      if(moneda === '') return;
-      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
-      const resultado = await axios.get(url);
-      guardarCargardo(true);
-      setTimeout(()=>{
-        guardarCargardo(false);
-      },3000)
-    }
-    cotizarCriptomoneda();
-  },[criptomoneda,moneda])
+  const [ moneda, guardarMoneda] = useState('');
+  const [ criptomoneda, guardarCriptomoneda] = useState('');
+  const [ cargando, guardarCargando ] = useState(false);
+  const [ resultado, guardarResultado ] = useState({});
 
-  //Mostrar Spinner
-  const componente = (cargando) ? <Spinner /> : null;
+  useEffect(() => {
+      const cotizarCriptomoneda = async () => {
+
+        // si no hay moneda, no ejecutar
+        if(moneda === '') return;
+
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+        const resultado = await axios.get(url);
+
+        // mostrar spinner
+        guardarCargando(true);
+
+        // ocultar spinner y agregar el resultado
+        setTimeout(() => {
+          guardarCargando(false);
+          guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+        }, 3000);
+
+      }
+
+      cotizarCriptomoneda();
+  }, [ criptomoneda, moneda ]);
+
+  // Mostrar Spinner o resultado
+  const componente = (cargando) ? <Spinner /> : <Cotizacion resultado={resultado} />
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="one-half column">
-          <img src={imagen} alt="imagen criptomoneda" className="logotipo"/>
+        <div className="row">
+            <div className="one-half column">
+                <img src={imagen} alt="imagen criptomonedas" className="logotipo" />
+            </div>
+            <div className="one-half column">
+                <h1>Cotiza Criptomonedas al Instante</h1>
+
+                <Formulario 
+                  guardarMoneda={guardarMoneda}
+                  guardarCriptomoneda={guardarCriptomoneda}
+                />
+
+                {componente}
+            </div>
         </div>
-        <div className="one-half column">
-          <h1>Cotiza Criptomonedas al instante</h1>
-          <Formulario 
-          guardarMoneda={guardarMoneda}
-          guardarCriptomoneda={guardarCriptomoneda}/>
-          {componente}
-        </div>
-      </div>
     </div>
   );
 }
